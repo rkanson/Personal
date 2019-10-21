a9afec238ca9c684ec22067c45e6ac73b864af84
Rich Kanson <richardkanson@gmail.com>
2019-10-21 10:22:13 -0400

    wedding page 301

---
+++ pages/wedding.js
@@ -0,0 +1,12 @@
+function Wedding() {
+	return (
+		<>
+			<script>
+				location.href =
+				'https://www.weddingwire.us/website/richard-kanson-and-erika-longfield';
+			</script>
+		</>
+	);
+}
+
+export default Wedding;
